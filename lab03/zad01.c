c8bb02648f83a2077065b601280e0a7eeeb23684
Msegun <maslo.14@o2.pl>
2016-03-10 18:26:52 +0100

    zad01 lab03 i tabela readme

--- lab03/zad01.c
+++ lab03/zad01.c
@@ -13,7 +13,7 @@ int main()
       return 0;
   }
   fprintf( plik , "|TYP     |     wartosc dolna|      wartosc gorna|      ziarno|     precyzja|     format we/wy|\n" );
-  fprintf( plik , "|--------------------------------------------------------------------------------------------|\n" );
+  fprintf( plik , "|--------|------------------|-------------------|------------|-------------|----------------------|\n" );
   fprintf( plik , "|Short   |     %i|     %i|     |     |     i|\n",SHRT_MIN , SHRT_MAX );
   fprintf( plik , "|Int     |     %i|     %i|     |     |     i|\n", INT_MIN , INT_MAX );
   fprintf( plik , "|Long    |     %li|     %li|     |     |     li|\n", LONG_MIN , LONG_MAX );
@@ -21,7 +21,6 @@ int main()
   fprintf( plik , "|Float    |     %e|     %e|     %e|     %i|     e f|\n", FLT_MIN , FLT_MAX , FLT_EPSILON , FLT_DIG  );
   fprintf( plik , "|Double    |     %le|     %le|     %le|     %i|     le lf|\n", DBL_MIN , DBL_MAX , DBL_EPSILON , DBL_DIG  );
   fprintf( plik , "|Long double    |     %Le|     %Le|     %Le|     %i|     le lf|\n", LDBL_MIN , LDBL_MAX , LDBL_EPSILON , LDBL_DIG  );
-  fprintf( plik , "|--------------------------------------------------------------------------------------------|\n" );
   fclose(plik);
 
   return 0;
