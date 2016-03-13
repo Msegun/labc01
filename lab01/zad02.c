be40ef0a1e4d53190e04d1c91c741bfc8252514c
Msegun <maslo.14@o2.pl>
2016-03-13 19:22:18 +0100

    Update zad02.c

--- lab01/zad02.c
+++ lab01/zad02.c
@@ -1,4 +1,7 @@
+//for ( ; (z=getchar())!=EOF; printf("%c",z)); 
+//Napisać ,,czysty'' program, czyli bez efektów ubocznych, działający tak samo jak powyższa pętla for. 
 #include <stdio.h>
+
 //napisac ten program bez dodatkowych zmiennych
 int main ()
 {
