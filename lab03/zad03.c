4328b3ec7c9e8a5f977d9220d3aaf77d4dc509c8
Msegun <maslo.14@o2.pl>
2016-03-18 00:11:23 +0100

    komentarze do odpowiedzi zad03.c lab03

--- lab03/zad03.c
+++ lab03/zad03.c
@@ -10,8 +10,15 @@
 
 int main() {
   printf("Rzutowanie FLT_MAX do (int)FLT_MAX: '%e' do '%i'\n", FLT_MAX, (int)FLT_MAX);
+  //Wypisze liczbe najbliższą zadanemu floatowi w innym typie int. W tym wypadku otrzymamy maksymalny int
   printf("Rzutowanie DBM_MAX do (int)DBL_MAX: '%e' do '%i'\n", DBL_MAX, (int)DBL_MAX);
+  //Tutaj podobnie otrzymamy maksymalny int. Double jest wieksze od float
   printf("Rzutowanie LDBL_MAX do (double)LDBL_MAX: '%Le' do '%e'\n", LDBL_MAX, (double)LDBL_MAX);
+  //trzymany wynik 'inf' prawdopodobnie świadczy o tym ze różnica miedzy LDBL_MAX a DBL_MAX
+  //jest tak gigantyczna ze kompilator upraszcza wypisując infinity
   printf("Rzutowanie INT_MAX do (float)FLT_MAX: '%i' do '%e'\n", INT_MAX, (float)INT_MAX);
+  //Pokaże nam odpowiednik INT_MAX po rzutowaniu na float ponieważ float>int dodatkowo pokaże ja z dana potęgą po przecinku
   printf("Rzutowanie LLONG_MAX do (double)LLONG_MAX: '%lli' do '%18.1lf'\n", LLONG_MAX, (double)LLONG_MAX);
+  //Reprezentacja LLONG_MAX przerobione do typu double
+
 }
